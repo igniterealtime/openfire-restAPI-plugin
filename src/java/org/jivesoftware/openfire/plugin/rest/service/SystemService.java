a422b8cc0057c955f9a55198dc2e3de9617ef3b5
Guus der Kinderen <guus.der.kinderen@gmail.com>
2022-02-24 20:40:30 +0100

    fixes #92: Add endpoints for 'readiness' and 'liveness'
    Adds endpoints that perform checks for 'readiness' of Openfire (to accept data), and 'liveness' (to check if Openfire is in a state where it needs a reboot). See issue #92 for more details.
    
    An endpoint is added for each individual check. Additionally, and endpoint has also been added that performs all checks of a particular type.

--- src/java/org/jivesoftware/openfire/plugin/rest/service/SystemService.java
+++ src/java/org/jivesoftware/openfire/plugin/rest/service/SystemService.java
@@ -27,6 +27,7 @@ import org.jivesoftware.openfire.plugin.rest.controller.SystemController;
 import org.jivesoftware.openfire.plugin.rest.entity.SystemProperties;
 import org.jivesoftware.openfire.plugin.rest.entity.SystemProperty;
 import org.jivesoftware.openfire.plugin.rest.exceptions.ServiceException;
+import org.jivesoftware.openfire.spi.ConnectionType;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -112,6 +113,162 @@ public class SystemService {
         throws ServiceException
     {
         SystemController.getInstance().deleteSystemProperty(propertyKey);
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/liveness")
+    @Operation( summary = "Perform all liveness checks",
+        description = "Detects if Openfire has reached a state that it cannot recover from, except for with a restart, based on every liveness check that it has implemented.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "The system is live."),
+            @ApiResponse(responseCode = "503", description = "At least one liveness check failed: the system is determined to not be alive.")
+        })
+    public Response liveness() {
+        // Try to execute these in the order of resource usage.
+        if (SystemController.getInstance().hasDeadlock()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        if (SystemController.getInstance().hasSystemPropertyRequiringRestart()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/liveness/deadlock")
+    @Operation( summary = "Perform 'deadlock' liveness check.",
+        description = "Detects if Openfire has reached a state that it cannot recover from because of a deadlock.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "The system is live."),
+            @ApiResponse(responseCode = "503", description = "A deadlock is detected.")
+        })
+    public Response livenessDeadlock() {
+        // Try to execute these in the order of resource usage.
+        if (SystemController.getInstance().hasDeadlock()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/liveness/properties")
+    @Operation( summary = "Perform 'properties' liveness check.",
+        description = "Detects if Openfire has reached a state that it cannot recover from because a system property change requires a restart to take effect.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "The system is live."),
+            @ApiResponse(responseCode = "503", description = "One or more system property changes that require a server restart have been detected.")
+        })
+    public Response livenessSystemProperties() {
+        // Try to execute these in the order of resource usage.
+        if (SystemController.getInstance().hasSystemPropertyRequiringRestart()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/readiness")
+    @Operation( summary = "Perform all readiness checks",
+        description = "Detects if Openfire is in a state where it is ready to process traffic, based on every readiness check that it has implemented.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "The system is ready."),
+            @ApiResponse(responseCode = "503", description = "At least one readiness check failed: the system is determined to not be able to process traffic.")
+        })
+    public Response readiness() {
+        // Try to execute these in the order of resource usage.
+        if (!SystemController.getInstance().isStarted()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        if (!SystemController.getInstance().hasClusteringStartedWhenEnabled()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        if (!SystemController.getInstance().hasPluginManagerExecuted()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        if (!SystemController.getInstance().areConnectionListenersStarted()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/readiness/server")
+    @Operation( summary = "Perform 'server started' readiness check",
+        description = "Detects if Openfire's core service has been started.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "The system is ready."),
+            @ApiResponse(responseCode = "503", description = "The Openfire service has not finished starting up yet.")
+        })
+    public Response readinessServerStart() {
+        // Try to execute these in the order of resource usage.
+        if (!SystemController.getInstance().isStarted()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/readiness/cluster")
+    @Operation( summary = "Perform 'cluster' readiness check",
+        description = "Detects if the cluster functionality has finished starting (or is disabled).",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "The system is ready."),
+            @ApiResponse(responseCode = "503", description = "Clustering functionality is enabled, but has not finished starting up yet.")
+        })
+    public Response readinessCluster() {
+        // Try to execute these in the order of resource usage.
+        if (!SystemController.getInstance().hasClusteringStartedWhenEnabled()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/readiness/plugins")
+    @Operation( summary = "Perform 'plugins' readiness check",
+        description = "Detects if Openfire has finished starting its plugins.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "The system is ready."),
+            @ApiResponse(responseCode = "503", description = "Plugins have not all been started yet.")
+        })
+    public Response readinessPlugins() {
+        // Try to execute these in the order of resource usage.
+        if (!SystemController.getInstance().hasPluginManagerExecuted()) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/readiness/connections")
+    @Operation( summary = "Perform 'connections' readiness check",
+        description = "Detects if Openfire is ready accepting connection requests.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "The system is ready."),
+            @ApiResponse(responseCode = "400", description = "The provided connectionType value is invalid."),
+            @ApiResponse(responseCode = "503", description = "Openfire currently does not accept (all) connections.")
+        })
+    public Response readinessConnections(
+        @Parameter(description = "Optional. Use to limit the check to one particular connection type. One of: SOCKET_S2S, SOCKET_C2S, BOSH_C2S, WEBADMIN, COMPONENT, CONNECTION_MANAGER", example = "SOCKET_C2S", required = false) @QueryParam("connectionType") String connectionType,
+        @Parameter(description = "Check the encrypted (true) or unencrypted (false) variant of the connection type. Only used in combination with 'connectionType'.", required = false) @QueryParam("servicename") Boolean encrypted
+    ) {
+        if (connectionType != null && !connectionType.isEmpty()) {
+            // Limit check to one connection listener.
+            try {
+                ConnectionType limitToType = ConnectionType.valueOf(connectionType);
+                if (!SystemController.getInstance().isConnectionListenerStartedWhenEnabled(limitToType, encrypted)) {
+                    return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+                }
+            } catch (Throwable t) {
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+        } else {
+            // Check all connection listeners.
+            if (!SystemController.getInstance().areConnectionListenersStarted()) {
+                return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+            }
+        }
         return Response.status(Response.Status.OK).build();
     }
 }
