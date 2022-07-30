fa16a096f641e314e1ae6bfbc1a38b09922cae90
Emiel van der Herberg <evdherberg@gmail.com>
2022-07-30 22:44:36 +0200

    #127 Improved RoomCreationResultEntities to be sorted by type of result

--- src/java/org/jivesoftware/openfire/plugin/rest/entity/RoomCreationResultEntities.java
+++ src/java/org/jivesoftware/openfire/plugin/rest/entity/RoomCreationResultEntities.java
@@ -16,27 +16,74 @@
 
 package org.jivesoftware.openfire.plugin.rest.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
 import java.util.List;
 
 @XmlRootElement(name = "results")
+@XmlType(propOrder = { "successResults", "failureResults", "otherResults" })
 public class RoomCreationResultEntities {
-    List<RoomCreationResultEntity> results;
+    List<RoomCreationResultEntity> successResults;
+    List<RoomCreationResultEntity> failureResults;
+
+    // This last list is for if a new result type is defined, but no extra result list is added here - a "catch all"
+    List<RoomCreationResultEntity> otherResults;
 
     public RoomCreationResultEntities() {
+        this.successResults = new ArrayList<>();
+        this.failureResults = new ArrayList<>();
+        this.otherResults = new ArrayList<>();
     }
 
     public RoomCreationResultEntities(List<RoomCreationResultEntity> results) {
-        this.results = results;
+        this();
+        addResults(results);
+    }
+
+    public void addResults(List<RoomCreationResultEntity> resultsToAdd) {
+        resultsToAdd.forEach(this::addResult);
+    }
+
+    public void addResult(RoomCreationResultEntity resultToAdd) {
+        switch (resultToAdd.getResultType()) {
+            case Success:
+                this.successResults.add(resultToAdd);
+                break;
+            case Failure:
+                this.failureResults.add(resultToAdd);
+                break;
+            default:
+                this.otherResults.add(resultToAdd);
+        }
     }
 
     @XmlElement(name = "result")
-    public List<RoomCreationResultEntity> getResults() {
-        return results;
+    @XmlElementWrapper(name = "success")
+    @JsonProperty(value = "success")
+    @Schema(description = "All creation results of type success")
+    public List<RoomCreationResultEntity> getSuccessResults() {
+        return successResults;
     }
 
-    public void setResults(List<RoomCreationResultEntity> results) {
-        this.results = results;
+    @XmlElement(name = "result")
+    @XmlElementWrapper(name = "failure")
+    @JsonProperty(value = "failure")
+    @Schema(description = "All creation results of type failure")
+    public List<RoomCreationResultEntity> getFailureResults() {
+        return failureResults;
+    }
+
+    @XmlElement(name = "result")
+    @XmlElementWrapper(name = "other")
+    @JsonProperty(value = "other")
+    @Schema(description = "All creation results of a type other than success or failure")
+    public List<RoomCreationResultEntity> getOtherResults() {
+        return otherResults;
     }
 }
